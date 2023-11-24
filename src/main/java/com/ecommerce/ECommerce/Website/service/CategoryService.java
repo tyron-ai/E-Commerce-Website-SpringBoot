486896d7742d261ce33f2bd7294e821b510f3cef
Tyron <tyronnyambe@gmail.com>
2023-11-24 21:02:34 +0200

    Implemented editCategory method

--- src/main/java/com/ecommerce/ECommerce/Website/service/CategoryService.java
+++ src/main/java/com/ecommerce/ECommerce/Website/service/CategoryService.java
@@ -2,10 +2,10 @@ package com.ecommerce.ECommerce.Website.service;
 
 import com.ecommerce.ECommerce.Website.model.Category;
 import com.ecommerce.ECommerce.Website.repository.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class CategoryService {
@@ -25,5 +25,24 @@ public class CategoryService {
     public List<Category> listCategories()
     {
        return categoryRepository.findAll();
+    }
+
+    public void editCategory(Integer id, Category updateCategory) {
+
+        Category category=categoryRepository.getReferenceById(id);
+
+        if (category == null) {
+            throw new NoSuchElementException("Category not found with ID: " + id);
+        }
+
+        category.setCategoryName(updateCategory.getCategoryName());
+        category.setDescription(updateCategory.getDescription());
+        category.setImageUrl(updateCategory.getImageUrl());
+        categoryRepository.save(category);
+
+    }
+
+    public Boolean findById(Integer id) {
+        return categoryRepository.findById(id).isPresent();
     }
 }
