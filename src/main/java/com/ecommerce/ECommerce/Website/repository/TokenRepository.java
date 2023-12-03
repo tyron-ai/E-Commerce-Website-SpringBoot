81f7f4b8bc3c4337b7b409b8e8c721fb5a2b4595
Tyron <tyronnyambe@gmail.com>
2023-12-03 09:54:59 +0200

    findByUser

--- src/main/java/com/ecommerce/ECommerce/Website/repository/TokenRepository.java
+++ src/main/java/com/ecommerce/ECommerce/Website/repository/TokenRepository.java
@@ -1,10 +1,12 @@
 package com.ecommerce.ECommerce.Website.repository;
 
 import com.ecommerce.ECommerce.Website.model.AuthenticationToken;
-import io.swagger.models.auth.In;
+import com.ecommerce.ECommerce.Website.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TokenRepository extends JpaRepository<AuthenticationToken, In> {
+public interface TokenRepository extends JpaRepository<AuthenticationToken, Integer> {
+
+    AuthenticationToken findByUser(User user);
 }
