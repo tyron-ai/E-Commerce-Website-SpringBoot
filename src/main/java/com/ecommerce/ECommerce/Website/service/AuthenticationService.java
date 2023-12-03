db93df36293fc6321ce5c7cf703f31177893feef
Tyron <tyronnyambe@gmail.com>
2023-12-03 09:48:39 +0200

    saveToken and getToken

--- src/main/java/com/ecommerce/ECommerce/Website/service/AuthenticationService.java
+++ src/main/java/com/ecommerce/ECommerce/Website/service/AuthenticationService.java
@@ -1,8 +1,23 @@
 package com.ecommerce.ECommerce.Website.service;
 
+import com.ecommerce.ECommerce.Website.model.AuthenticationToken;
+import com.ecommerce.ECommerce.Website.model.User;
+import com.ecommerce.ECommerce.Website.repository.TokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
 
+    @Autowired
+    TokenRepository tokenRepository;
+
+
+    public void saveToken(AuthenticationToken userToken) {
+    tokenRepository.save(userToken);
+    }
+
+    public AuthenticationToken getToken(User user) {
+        return tokenRepository.findByUser(user);
+    }
 }
