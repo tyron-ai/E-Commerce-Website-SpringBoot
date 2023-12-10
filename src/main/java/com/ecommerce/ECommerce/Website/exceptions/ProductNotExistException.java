277611ca9d51eea3b6c933c0d02e7b7ce2da14aa
Tyron <tyronnyambe@gmail.com>
2023-12-10 09:06:23 +0200

    productNotExistException

---
+++ src/main/java/com/ecommerce/ECommerce/Website/exceptions/ProductNotExistException.java
@@ -0,0 +1,10 @@
+package com.ecommerce.ECommerce.Website.exceptions;
+
+public class ProductNotExistException extends IllegalArgumentException {
+    public ProductNotExistException(String message) {
+
+        super(message);
+
+
+    }
+}
